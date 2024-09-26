dbcd14dcbc6e2ea6a1dc839fcd7583a50dda6bb2
Ozsumit <129023326+Ozsumit@users.noreply.github.com>
2024-09-26 23:56:41 +0545

    Update game.tsx

--- components/ui/game.tsx
+++ components/ui/game.tsx
@@ -409,10 +409,10 @@ const DonationClicker: React.FC = () => {
         return {
           ...prev,
           donations: prev.donations - prev.autoClickerCost,
-          autoClickerCount: prev.autoClickerCount + 0.4,
+          autoClickerCount: prev.autoClickerCount + .4,
           autoClickerCost: newAutoClickerCost,
-          autoClickerby: prev.autoClickerCount + 1, // Updated this line
-          autoClickerLevel: prev.autoClickerCount + 1,
+          autoClickerby: prev.autoClickerCount + .4, // Updated this line
+          autoClickerLevel: prev.autoClickerCount + .6,
         };
       }
       return prev;
