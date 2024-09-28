6b811e5bbd4623c440d92bd0ca5c6e93a37b6f03
Ozsumit <oniwagon>
2024-09-28 20:32:40 +0545

    NB

--- next.config.mjs
+++ next.config.mjs
@@ -1,65 +1,55 @@
-// next.config.mjs
 import withPWA from "next-pwa";
 
 const runtimeCaching = [
   {
-    urlPattern: /^https?.*/,
-    handler: "CacheFirst",
+    urlPattern: /^https?.*/, // Cache all requests to external APIs
+    handler: "NetworkFirst",
     options: {
       cacheName: "external-cache",
       expiration: {
         maxEntries: 50,
-        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+        maxAgeSeconds: 24 * 60 * 60, // 1 day
       },
     },
   },
   {
     urlPattern: "/",
-    handler: "CacheFirst",
+    handler: "NetworkFirst", // Serve from network first, fallback to cache
     options: {
       cacheName: "homepage-cache",
       expiration: {
         maxEntries: 1,
-        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+        maxAgeSeconds: 24 * 60 * 60, // 1 day
       },
     },
   },
   {
     urlPattern: /\/_next\/image/,
-    handler: "CacheFirst",
+    handler: "CacheFirst", // Cache optimized images
     options: {
       cacheName: "image-cache",
       expiration: {
         maxEntries: 100,
-        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+        maxAgeSeconds: 7 * 24 * 60 * 60, // 1 week
       },
     },
   },
   {
-    urlPattern: /\/page\.tsx/,
-    handler: "CacheFirst",
+    urlPattern: /\/page.tsx/,
+    handler: "CacheFirst", // Offline fallback page
     options: {
       cacheName: "offline-cache",
       expiration: {
         maxEntries: 1,
-        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
       },
     },
   },
 ];
 
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  // Add other Next.js config options here
-};
-
-const withPWAConfig = withPWA({
+export default withPWA({
   dest: "public",
-  disable: process.env.NODE_ENV === "development",
+  runtimeCaching,
   register: true,
   skipWaiting: true,
-  runtimeCaching,
+  //   disable: process.env.NODE_ENV === "development", // Enable PWA only in production
 });
-
-export default withPWAConfig(nextConfig);
