cd83afde615aa471fd6b7e93b8211901910276f7
Ozsumit <oniwagon>
2024-09-28 20:26:43 +0545

    7

--- next.config.mjs
+++ next.config.mjs
@@ -1,35 +1,65 @@
-const withPWA = require("next-pwa")({
+// next.config.mjs
+import withPWA from "next-pwa";
+
+const runtimeCaching = [
+  {
+    urlPattern: /^https?.*/,
+    handler: "CacheFirst",
+    options: {
+      cacheName: "external-cache",
+      expiration: {
+        maxEntries: 50,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+      },
+    },
+  },
+  {
+    urlPattern: "/",
+    handler: "CacheFirst",
+    options: {
+      cacheName: "homepage-cache",
+      expiration: {
+        maxEntries: 1,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+      },
+    },
+  },
+  {
+    urlPattern: /\/_next\/image/,
+    handler: "CacheFirst",
+    options: {
+      cacheName: "image-cache",
+      expiration: {
+        maxEntries: 100,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+      },
+    },
+  },
+  {
+    urlPattern: /\/page\.tsx/,
+    handler: "CacheFirst",
+    options: {
+      cacheName: "offline-cache",
+      expiration: {
+        maxEntries: 1,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+      },
+    },
+  },
+];
+
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // Add other Next.js config options here
+};
+
+const withPWAConfig = withPWA({
   dest: "public",
-  runtimeCaching: [
-    {
-      urlPattern: /^https?.*/, // External APIs
-      handler: "NetworkFirst",
-      options: {
-        cacheName: "external-cache",
-        expiration: {
-          maxEntries: 50,
-          maxAgeSeconds: 24 * 60 * 60, // 1 day
-        },
-      },
-    },
-    {
-      urlPattern: /\/_next\/image/, // Images caching
-      handler: "CacheFirst",
-      options: {
-        cacheName: "image-cache",
-        expiration: {
-          maxEntries: 100,
-          maxAgeSeconds: 7 * 24 * 60 * 60, // 1 week
-        },
-      },
-    },
-  ],
-  disable: process.env.NODE_ENV === "development" || isExport,
+  disable: process.env.NODE_ENV === "development",
   register: true,
   skipWaiting: true,
+  runtimeCaching,
 });
 
-module.exports = withPWA({
-  output: "export", // ensures next export is compatible
-  trailingSlash: true, // GitHub Pages sometimes requires this for proper routing
-});
+export default withPWAConfig(nextConfig);
